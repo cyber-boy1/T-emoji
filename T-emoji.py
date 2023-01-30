95e6959d7816c84a0abeac8b05d24acbdb7d565d
CYBER-BOY <112181495+cyber-boy1@users.noreply.github.com>
2023-01-30 14:07:47 +0330

    Add files via upload

---
+++ T-emoji.py
@@ -0,0 +1,268 @@
+from sys import exit
+from colorama import Fore
+from os import system
+from time import sleep
+print (Fore.CYAN, "WELCOME!!")
+system("pip install coloerama")
+system("clear")
+
+sleep(1)
+CB= (Fore.CYAN,"  ___ _   _| |__   ___ _ __  | |__   ___  _   _",
+" / __| | | |  _ \ / _ \  __| |  _ \ / _ \| | | |",
+"| (__| |_| | |_) |  __/ |    | |_) | (_) | |_| |",
+" \___|\__, |_.__/ \___|_|    |_.__/ \___/ \__, |",
+"      |___/                               |___/")
+for CB in CB:
+ print (CB)
+ sleep(0.3)
+print ()
+print (Fore.RED, "         1= toilet")
+print ("               2= cyber boy")
+print (Fore.BLUE,"")
+
+input= input('>>>>>')
+
+if input=='1':
+ system("pkg install toilet")
+ system("clear")
+ system("toilet -f mono12 -F gay")
+print ()
+
+if input=='2':
+ 
+ sleep(1)
+A= (Fore.RED,"    ▄▄","   ████","   ████","   █  █","  ██████"," ▄██  ██▄"," ▀▀    ▀▀")
+for A in A:
+ print (A)
+ sleep(0.1)
+print ()
+
+B= (Fore.YELLOW," ▄▄▄▄▄▄"," ██▀▀▀▀██", " ██    ██", " ████████"," ██    ██"," ██▄▄▄▄██"," ▀▀▀▀▀▀▀")
+for B in B:
+ print (B)
+ sleep(0.1)
+print ()
+
+C= (Fore.GREEN,"     ▄▄▄▄","  ██▀▀▀▀█"," ██▀"," ██"," ██▄","  ██▄▄▄▄█")
+
+for C in C:
+ print (C)
+ sleep(0.1)
+print ()
+
+D= (Fore.BLUE," ▄▄▄▄▄"," ██▀▀▀██"," ██    ██"," ██    ██"," ██    ██"," ██▄▄▄██"," ▀▀▀▀▀")
+for D in D:
+ print (D)
+ sleep(0.1)
+print ()
+
+E= (Fore.MAGENTA," ▄▄▄▄▄▄▄▄"," ██▀▀▀▀▀▀"," ██"," ███████"," ██"," ██▄▄▄▄▄▄"," ▀▀▀▀▀▀▀▀")
+for E in E:
+ print (E)
+ sleep(0.1)
+print ()
+
+F= (Fore.GREEN," ▄▄▄▄▄▄▄▄"," ██▀▀▀▀▀▀"," ██"," ███████"," ██"," ██"," ▀▀")
+for F in F:
+ print (F)
+ sleep(0.1)
+print ()
+
+G= (Fore.BLUE,"    ▄▄▄▄","  ██▀▀▀▀█"," ██"," ██  ▄▄▄▄"," ██  ▀▀██","  ██▄▄▄██","    ▀▀▀▀")
+for G in G:
+ print (G)
+ sleep(0.1)
+print ()
+
+H= (Fore.YELLOW," ▄▄    ▄▄"," ██    ██"," ██    ██"," ████████"," ██    ██"," ██    ██"," ▀▀    ▀▀")
+for H in H:
+ print (H)
+ sleep(0.1)
+print ()
+
+I= (Fore.MAGENTA,"  ▄▄▄▄▄▄","  ▀▀██▀▀","    ██","    ██","    ██","  ▄▄██▄▄","  ▀▀▀▀▀▀")
+for I in I:
+ print (I)
+ sleep(0.1)
+print ()
+
+J= (Fore.RED,"    ▄▄▄▄▄",
+"    ▀▀▀██",
+"       ██",
+"       ██",
+"       ██",
+" █▄▄▄▄▄██",
+"  ▀▀▀▀▀")
+for J in J:
+ print (J)
+ sleep(0.1)
+print ()
+
+K= (Fore.BLUE," ▄▄   ▄▄▄"," ██  ██▀"," ██▄██"," █████"," ██  ██▄"," ██   ██▄"," ▀▀    ▀▀")
+for K in K:
+ print (K)
+ sleep(0.1)
+print ()
+
+L= (Fore.YELLOW," ▄▄"," ██"," ██"," ██"," ██"," ██▄▄▄▄▄▄"," ▀▀▀▀▀▀▀▀")
+for L in L:
+ print (L)
+ sleep(0.1)
+print ()
+
+M= (Fore.MAGENTA," ▄▄▄  ▄▄▄"," ███  ███"," ████████"," ██ ██ ██"," ██ ▀▀ ██"," ██    ██"," ▀▀    ▀▀")
+for M in M:
+ print (M)
+ sleep(0.1)
+print ()
+
+N= (Fore.GREEN," ▄▄▄   ▄▄"," ███   ██"," ██▀█  ██"," ██ ██ ██"," ██  █▄██"," ██   ███"," ▀▀   ▀▀▀")
+for N in N:
+ print (N)
+ sleep(0.1)
+print ()
+
+O= (Fore.BLUE,"  ▄▄▄▄","  ██▀▀██"," ██    ██"," ██    ██"," ██    ██","  ██▄▄██","   ▀▀▀▀")
+for O in O:
+ print (O)
+ sleep(0.1)
+print ()
+
+P= (Fore.CYAN,"▄▄▄▄▄▄","██▀▀▀▀█▄","██    ██","██████▀","██","██","▀▀")
+for P in P:
+ print (P)
+ sleep(0.1)
+print ()
+
+Q= (Fore.GREEN,"   ▄▄▄▄",
+"  ██▀▀██",
+" ██    ██",
+" ██    ██",
+" ██    ██",
+"  ██▄▄██▀",
+"   ▀▀▀██")
+for Q in Q:
+ print (Q)
+ sleep(0.1)
+print ()
+
+R= (Fore.MAGENTA," ▄▄▄▄▄▄",
+" ██▀▀▀▀██",
+" ██    ██",
+" ███████",
+" ██  ▀██▄",
+" ██    ██",
+" ▀▀    ▀▀▀")
+for R in R:
+ print (R)
+ sleep(0.1)
+print ()
+
+S= (Fore.YELLOW,"   ▄▄▄▄",
+" ▄█▀▀▀▀█",
+" ██▄",
+"  ▀████▄",
+"      ▀██",
+" █▄▄▄▄▄█▀",
+"  ▀▀▀▀▀")
+for S in S:
+ print (S)
+ sleep(0.1)
+print ()
+
+T= (Fore.CYAN," ▄▄▄▄▄▄▄▄",
+" ▀▀▀██▀▀▀",
+"    ██",
+"    ██",
+"    ██",
+"    ██",
+"    ▀▀")
+for T in T:
+ print (T)
+ sleep(0.1)
+print ()
+
+U= (Fore.RED," ▄▄    ▄▄",
+" ██    ██",
+" ██    ██",
+" ██    ██",
+" ██    ██",
+" ▀██▄▄██▀",
+"   ▀▀▀▀")
+for U in U:
+ print (U)
+ sleep(0.1)
+print ()
+
+V= (Fore.MAGENTA," ▄▄    ▄▄",
+" ▀██  ██▀",
+"  ██  ██",
+"  ██  ██",
+"   ████",
+"   ████",
+"   ▀▀▀▀")
+for V in V:
+ print (V)
+ sleep(0.1)
+print ()
+
+W= (Fore.CYAN,"▄▄      ▄▄",
+"██      ██",
+"▀█▄ ██ ▄█▀",
+" ██ ██ ██",
+" ███▀▀███",
+" ███  ███",
+" ▀▀▀  ▀▀▀")
+for W in W:
+ print (W)
+ sleep(0.1)
+print ()
+
+X= (Fore.GREEN," ▄▄▄  ▄▄▄",
+"  ██▄▄██",
+"   ████",
+"    ██",
+"   ████",
+"  ██  ██",
+" ▀▀▀  ▀▀▀")
+for X in X:
+ print (X)
+ sleep(0.1)
+print ()
+
+Y= (Fore.RED,"▄▄▄    ▄▄▄",
+" ██▄  ▄██",
+"  ██▄▄██",
+"   ▀██▀",
+"    ██",
+"    ██",
+"    ▀▀")
+for Y in Y:
+ print (Y)
+ sleep(0.1)
+print ()
+
+Z= (Fore.BLUE," ▄▄▄▄▄▄▄▄",
+" ▀▀▀▀▀███",
+"     ██▀",
+"   ▄██▀",
+"  ▄██",
+" ███▄▄▄▄▄",
+" ▀▀▀▀▀▀▀▀")
+for Z in Z:
+ print (Z)
+ sleep(0.1)
+print ()
+
+sleep(1)
+print (Fore.YELLOW, "")
+print (Fore.YELLOW, "")
+print (Fore.YELLOW, "")
+print (Fore.YELLOW, "")
+print (Fore.YELLOW, "")
+print (Fore.YELLOW, "")
+
+print ("                                      THE END")
+
+exit(0)
+
